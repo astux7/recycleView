83aa8a5aefb5d0ee7f24ed22f82d5aa6051fed51
Asta Bevainyte <asta.bevainyte@sky.uk>
2020-04-12 14:58:49 +0100

    5th commit - add recyclerAdapter

---
+++ app/src/main/java/com/example/recycleview/PetRecyclerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.recycleview
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recycleview.models.Pet
+
+class PetRecyclerAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    // declare data set using here
+    private var items: List<Pet> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
